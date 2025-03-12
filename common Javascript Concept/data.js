ce129129928c3a59525dd49aa5e40e0cbb206c99
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-03-12 14:46:59 +0600

    new file add some

---
+++ common Javascript Concept/data.js
@@ -0,0 +1,13 @@
+let person = "Sajjad Hossain JIM";
+let newPerson =  person;
+person = "Hit man";
+console.log(newPerson)
+
+
+
+let person1 = {name :"JIM"}
+let newPerson1 = person;
+person1 = "Hut man 2!!!"
+
+console.log(newPerson1)
+
---
+++ common Javascript Concept/scope.js
@@ -0,0 +1,13 @@
+function sum (num1 , num2 ){
+  let result = num1 + num2;
+  console.log(result)
+
+  if(true){
+    // var can access the result the value output show 
+    // var result1 = num1 - num2;
+    // Let / const cant not acess the block out of the bracket 
+    // let result1 = num1 - num2;
+  }
+//   console.log(result1)
+}
+console.log(sum(10,12))
